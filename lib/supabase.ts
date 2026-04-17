af6c14cce5c837357d271fb2342fbe4d23247775
blaq0rkid <blaq0rkid@users.noreply.github.com>
2026-04-17 11:27:51 -0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://rflkeeurnvljuteawrth.supabase.co'
+const supabaseAnonKey = 'sb_publishable_SBqxjMzzIXk5ymdChZVNmA_Suk35JlH'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
