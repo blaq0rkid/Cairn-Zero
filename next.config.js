d06378e777c14f83e22eae91ccab29085b03c4c7
blaq0rkid <blaq0rkid@users.noreply.github.com>
2026-04-21 12:21:23 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  output: 'export',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
