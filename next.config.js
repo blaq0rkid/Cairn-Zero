3d28a5e9ec449f978170866edea352aa01b83492
blaq0rkid <blaq0rkid@users.noreply.github.com>
2026-04-26 11:40:01 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
+  async redirects() {
+    return [
+      {
+        source: '/founder',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
   },
 }
 
